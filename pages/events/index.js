2f774e6a2bf08217937b884faaf28779ab2100f2
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-09 10:57:15 -0300

    feat: working on the all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,14 @@
 import { useRouter } from 'next/dist/client/router';
 
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
 
 import { EventList } from '../../components/events/EventList';
 import { EventSearch } from '../../components/events/EventSearch';
 
-export default function Events() {
-  const events = getAllEvents();
+export default function Events(props) {
   const router = useRouter();
+  
+  const { events } = props;
 
   function handleFindEvents(year, month) {
     const fullPath = `/events/${year}/${month}`
@@ -21,4 +22,15 @@ export default function Events() {
       <EventList events={events} />
     </>
   );
+}
+
+export async function getStaticProps () {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events
+    },
+    revalidate: 60
+  }
 }

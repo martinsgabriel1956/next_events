3c24774a59cd5591552da1d064b93d9c35ddf87d
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-09 11:54:10 -0300

    feat: using server-side rendering (SSR)

--- pages/events/[...slug]/index.js
+++ pages/events/[...slug]/index.js
@@ -5,14 +5,71 @@ import { ResultsTitle } from "../../../components/events/ResultsTitle";
 import { Button } from "../../../components/UI/Button";
 import { ErrorAlert } from "../../../components/UI/ErrorAlert";
 
-import { getFilteredEvents } from "../../../dummy-data";
+import { getFilteredEvents } from "../../../helpers/api-util";
 
-export default function AllEvents() {
+export default function AllEvents(props) {
   const router = useRouter();
 
-  const filteredData = router.query.slug;
+  const { hasError, events, date: {
+    year, month
+  } } = props;
 
-  if (!filteredData) return <div className="center">Loading ...</div>;
+  // const filteredData = router.query.slug;
+
+  // if (!filteredData) return <div className="center">Loading ...</div>;
+
+  // const filteredYear = filteredData[0];
+  // const filteredMonth = filteredData[1];
+
+  // const numYear = Number(filteredYear);
+  // const numMonth = Number(filteredMonth);
+
+  const invalidFieldDate =
+    hasError === true
+
+  if (invalidFieldDate) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
+  }
+
+  const filteredEvents = events;
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>  
+        </div>;
+      </>
+    );
+  }
+
+  const date = new Date(year, month - 1);
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList events={filteredEvents} />
+    </>
+  );
+}
+
+
+export async function getServerSideProps(ctx) {
+  const { params } = ctx;
+
+  const filteredData = params.slug;
 
   const filteredYear = filteredData[0];
   const filteredMonth = filteredData[1];
@@ -29,42 +86,25 @@ export default function AllEvents() {
     numMonth < 1;
 
   if (invalidFieldDate) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>Invalid filter. Please adjust your values!</p>
-        </ErrorAlert>
-        <div className="center">
-          <Button link="/events">Show All Events</Button>
-        </div>
-      </>
-    );
+    return {
+      props: {
+        hasError: true
+      },
+    }
   }
 
-  const filteredEvents = getFilteredEvents({
+  const filteredEvents = await getFilteredEvents({
     year: numYear,
     month: numMonth,
   });
 
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>No events found for the chosen filter!</p>
-        </ErrorAlert>
-        <div className="center">
-          <Button link="/events">Show All Events</Button>  
-        </div>;
-      </>
-    );
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: numYear,
+        month: numMonth,
+      }
+    }
   }
-
-  const date = new Date(numYear, numMonth - 1);
-
-  return (
-    <>
-      <ResultsTitle date={date} />
-      <EventList events={filteredEvents} />
-    </>
-  );
 }
